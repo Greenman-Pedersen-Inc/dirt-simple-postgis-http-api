32503d65ef311216e48a0af506503ac8f8a28eb5
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2022-01-13 19:29:12 +0000

    updated routes for route data

--- routes/geojson/route_histogram_geojson.js
+++ routes/geojson/route_histogram_geojson.js
@@ -1,44 +1,45 @@
 // route query
 const sql = (params, query) => {
+
+    // let query_parameters = JSON.parse(query.filter.replace( "'", '%27' ));
+    // let selected_sri = query_parameters.sri;
+
+    // delete query_parameters.sri;
+
+    // console.log(new URLSearchParams(query_parameters).toString())
+
+
     let formattedQuery = `
-        SELECT json_build_object(
-            'type', 'FeatureCollection',
-            'crs',  json_build_object(
-                'type',      'name', 
-                'properties', json_build_object(
-                    'name', 'EPSG:4326'  
-                )
-            ), 
-            'features', json_agg(
-                json_build_object(
-                    'type',       'Feature',
-                    'geometry',   ST_AsGeoJSON(geom)::json,
-                    'properties', json_build_object(
-                        'sri', sri,
-                        'mile_post', mp,
-                        'crashes', crashes
-                    )
-                )
-            )
-        ) as geojson
-        FROM (
-            SELECT
-                grouped_crash_data.*,
-                ST_Transform(geometry_join.geom, 4326) as geom
-            FROM (
-                SELECT segment_polygons.sri, segment_polygons.mp, count(*) as crashes from segment_polygons
-                left join ard_accidents_geom_partition
-                on segment_polygons.sri = ard_accidents_geom_partition.calc_sri
-                and segment_polygons.mp = ard_accidents_geom_partition.calc_milepost
-                where segment_polygons.sri = '${params.sri}'
-                ${query.filter ? ` AND ${query.filter}` : ''}
-                group by segment_polygons.sri, segment_polygons.mp
-            ) grouped_crash_data
-            left join segment_polygons geometry_join
-            on geometry_join.sri = grouped_crash_data.sri
-            and geometry_join.mp = grouped_crash_data.mp
-        ) features
-    `
+        with selected_crashes as (
+            select 
+                sri, 
+                milepost,
+                count(*) crash_count 
+            from ard_accidents_geom_partition
+            ${query_parameters ? ` where ${query.filter.replace('&', ' and ')}` : ''}
+            group by sri, milepost
+        ), route_data as (
+            select 
+                array[array[min(ST_XMin(st_transform(geom, 4326))), min(ST_YMin(st_transform(geom, 4326)))], array[max(ST_XMax(st_transform(geom, 4326))), max(ST_YMax(st_transform(geom, 4326)))]]
+            from segment_polygons
+            where sri = '${query_parameters.sri}'
+        )
+
+        select json_build_object(
+            'features', 
+				json_agg(
+					json_build_object(
+						'sri', sri,
+						'milepost', milepost,
+						'crashes', crash_count
+					)
+				),
+            'bbox', (select * from route_data),
+            'min_crashes', min(crash_count),
+            'max_crashes', max(crash_count),
+            'segments', count(*)
+        ) route_metrics from selected_crashes
+`
     console.log(formattedQuery);
     return formattedQuery;
 }
@@ -48,14 +49,13 @@ const schema = {
     description: 'Return table as GeoJSON.',
     tags: ['feature'],
     summary: 'return GeoJSON',
-    params: {
-        sri: {
-            type: 'string',
-            description: 'Name of the route that will be used for query.</em>'
-        }
-    },
+    params: {},
     querystring: {
         filter: {
+            type: 'string',
+            description: 'Optional filter parameters for a SQL WHERE statement.'
+        },
+        sri: {
             type: 'string',
             description: 'Optional filter parameters for a SQL WHERE statement.'
         }
@@ -65,37 +65,37 @@ const schema = {
 // create route
 module.exports = function(fastify, opts, next) {
     fastify.route({
-      method: 'GET',
-      url: '/routeHistogram/:sri',
-      schema: schema,
-      handler: function(request, reply) {
-        fastify.pg.connect(onConnect)
-  
-        function onConnect(err, client, release) {
-          if (err)
-            return reply.send({
-              statusCode: 500,
-              error: 'Internal Server Error',
-              message: 'unable to connect to database server'
-            })
-  
-          client.query(sql(request.params, request.query), function onResult(
-            err,
-            result
-          ) {
-            release()
-            if (err) {
-                reply.send(err)
-            } else if (result.rows && result.rows.length > 0) {
-                reply.send(result.rows[0].geojson)
-            } else {
-                reply.code(204);
+        method: 'GET',
+        url: '/geojson/routeHistogram',
+        schema: schema,
+        handler: function(request, reply) {
+            fastify.pg.connect(onConnect)
+
+            function onConnect(err, client, release) {
+                if (err)
+                    return reply.send({
+                        statusCode: 500,
+                        error: 'Internal Server Error',
+                        message: 'unable to connect to database server'
+                    })
+
+                client.query(sql(request.params, request.query), function onResult(
+                    err,
+                    result
+                ) {
+                    release()
+                    if (err) {
+                        reply.send(err)
+                    } else if (result.rows && result.rows.length > 0) {
+                        reply.send(result.rows[0].route_metrics)
+                    } else {
+                        reply.code(204);
+                    }
+                })
             }
-          })
         }
-      }
     })
     next()
-  }
+}
 
 module.exports.autoPrefix = '/'

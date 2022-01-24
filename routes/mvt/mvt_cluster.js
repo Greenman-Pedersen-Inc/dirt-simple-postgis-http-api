24c9b8b115289435245b6cf911847b667db45d11
snopachinda-gpi <snopachinda@gpinet.com>
2022-01-24 07:16:30 -0800

    fixed clusters to be faster and query correctly

--- routes/mvt/mvt_cluster.js
+++ routes/mvt/mvt_cluster.js
@@ -11,33 +11,40 @@ const sql = (params, query) => {
   } 
 
   let queryText = `
-        with complete_data as(
-            with crash_data as (
-                SELECT geom
-                FROM ard_accidents_geom_partition
-                WHERE ST_Intersects(geom, ST_Transform(ST_TileEnvelope(12,1193,1550), 4326))
-                -- Optional filter for the query input
-                ${whereClause ? ` AND ${whereClause}` : ''}
-            )
-            SELECT 
-                -- kmean, 
-                count(*) as crashes, 
-                ST_SetSRID(ST_Extent(geom_center), 4326) as bbox, 
-                ST_AsMVTGeom(
-                    ST_Transform(ST_SetSRID(ST_Centroid(ST_Extent(geom_center)), 4326), 3857),
-                    ST_TileEnvelope(${params.z}, ${params.x}, ${params.y})
-                ) as geom
-            FROM
-            (
-                -- Number indicates how many clusters should be created.
-                SELECT ST_ClusterKMeans(geom, ${22 - params.z}) OVER() AS kmean, ST_Centroid(geom) as geom_center
-                FROM ard_accidents_geom_partition
-                WHERE ST_Intersects(geom, ST_Transform(ST_TileEnvelope(${params.z}, ${params.x}, ${params.y}), 4326))
-            ) tsub
-            GROUP BY kmean
-        )
-        SELECT ST_AsMVT(complete_data.*, 'ard_accidents_geom_partition', 4096, 'geom') AS mvt from complete_data;
+      with complete_data as(
+        with crash_data as (
+            SELECT geom
+            FROM ard_accidents_geom_partition
+            WHERE ST_Intersects(geom, ST_Transform(ST_TileEnvelope(${params.z}, ${params.x}, ${params.y}), 4326))
+            -- Optional filter for the query input
+            ${whereClause ? ` AND ${whereClause}` : ''}
+        ), crash_count as (
+    select count(*) from crash_data
+    union all
+    select ${22 - params.z}
+  
+  )
+        SELECT
+            -- kmean,
+            count(*) as crashes,
+            ST_SetSRID(ST_Extent(geom_center), 4326) as bbox,
+            ST_AsMVTGeom(
+                ST_Transform(ST_SetSRID(ST_Centroid(ST_Extent(geom_center)), 4326), 3857),
+                ST_TileEnvelope(${params.z}, ${params.x}, ${params.y})
+            ) as geom
+        FROM
+        (
+            -- Number indicates how many clusters should be created.
+            SELECT ST_ClusterKMeans(geom, (select min(count) from crash_count)::INTEGER) OVER() AS kmean, ST_Centroid(geom) as geom_center
+            FROM crash_data
+        ) tsub
+        GROUP BY kmean
+    )
+    SELECT ST_AsMVT(complete_data.*, 'ard_accidents_geom_partition', 4096, 'geom') AS mvt from complete_data;
+
 `
+
+
   console.log(queryText);
   return queryText;
 }

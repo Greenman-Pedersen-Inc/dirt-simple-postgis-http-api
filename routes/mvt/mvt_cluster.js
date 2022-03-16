7cf9b618bcbf0360c639ed72c9380ae982a88ddd
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2022-03-16 16:43:29 -0400

    updates for better error handling

--- routes/mvt/mvt_cluster.js
+++ routes/mvt/mvt_cluster.js
@@ -163,6 +163,8 @@ module.exports = function (fastify, opts, next) {
 
             function onConnect(err, client, release) {
                 if (err) {
+                    release();
+
                     reply.send({
                         statusCode: 500,
                         error: 'Internal Server Error',
@@ -171,6 +173,8 @@ module.exports = function (fastify, opts, next) {
                 } else {
                     try {
                         if (request.query.selected_filters == undefined) {
+                            release();
+
                             reply.send({
                                 statusCode: 500,
                                 error: 'Internal Server Error',

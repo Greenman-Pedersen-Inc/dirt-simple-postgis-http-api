9b2bdfa5f1c587a011724a6f3483a645e03b6b13
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2022-04-22 10:28:22 -0400

    updates to reporting to stream file instead of passing hard link

--- routes/jurisdiction_report/jurisdiction_report.js
+++ routes/jurisdiction_report/jurisdiction_report.js
@@ -1,5 +1,5 @@
 // jurisdiction_report: generates the jurisdiction report pdf based on county, muni, start year, and end year inputs
-
+const fs = require('fs');
 const juriHelper = require('../../helper_functions/jurisdiction_report_helper');
 
 // *---------------*
@@ -23,22 +23,21 @@ const schema = {
         jurisdictionCode: {
             type: 'string',
             description: 'county code and muni code together',
-            default: "1102"
+            default: '1102'
         }
     }
-}
-
+};
 
 // *---------------*
 // create route
 // *---------------*
-module.exports = function(fastify, opts, next) {
+module.exports = function (fastify, opts, next) {
     fastify.route({
         method: 'GET',
         url: '/jurisidiction/report',
         schema: schema,
-        handler: function(request, reply) {
-            fastify.pg.connect(onConnect)
+        handler: function (request, reply) {
+            fastify.pg.connect(onConnect);
 
             function onConnect(err, client, release) {
                 if (err) {
@@ -49,45 +48,45 @@ module.exports = function(fastify, opts, next) {
                 var queryArgs = request.query;
                 if (queryArgs.startYear == undefined) {
                     return reply.send({
-                        "statusCode": 500,
-                        "error": "Internal Server Error",
-                        "message": "need start or end year"
+                        statusCode: 500,
+                        error: 'Internal Server Error',
+                        message: 'need start or end year'
                     });
                 } else if (queryArgs.endYear == undefined) {
                     return reply.send({
-                        "statusCode": 500,
-                        "error": "Internal Server Error",
-                        "message": "need start or end year"
+                        statusCode: 500,
+                        error: 'Internal Server Error',
+                        message: 'need start or end year'
                     });
                 } else if (queryArgs.jurisdictionCode == undefined) {
                     return reply.send({
-                        "statusCode": 500,
-                        "error": "Internal Server Error",
-                        "message": "need jurisdiction code"
+                        statusCode: 500,
+                        error: 'Internal Server Error',
+                        message: 'need jurisdiction code'
                     });
                 } else {
                     var reportQueries = juriHelper.getReportQueries(queryArgs);
                     var reportData = {
-                        "pedestrians": [],
-                        "drivers": [],
-                        "vehicles": [],
-                        "crashes": [],
-                        "police": [],
-                        "police2": []
+                        pedestrians: [],
+                        drivers: [],
+                        vehicles: [],
+                        crashes: [],
+                        police: [],
+                        police2: []
                     };
                     var promises = [];
                     var categories = [];
 
                     for (var key in reportQueries) {
                         if (reportQueries.hasOwnProperty(key)) {
-                            reportQueries[key].forEach(queryObj => {
+                            reportQueries[key].forEach((queryObj) => {
                                 const promise = new Promise((resolve, reject) => {
                                     try {
                                         const res = client.query(queryObj.query);
                                         return resolve(res);
                                     } catch (err) {
-                                        //console.log(err.stack);
-                                        // console.log(queryObj.query);
+                                        console.log(err.stack);
+                                        console.log(queryObj.query);
                                         return reject(error);
                                     }
                                 });
@@ -99,31 +98,49 @@ module.exports = function(fastify, opts, next) {
                         }
                     }
 
-                    Promise.all(promises).then((reportDataArray) => {
-                        for (let i = 0; i < reportDataArray.length; i++) {
-                            var data = reportDataArray[i].rows;
-                            var category = Object.keys(categories[i])[0];
-                            var tableTitle = Object.values(categories[i])[0];
-                            reportData[category].push({
-                                [tableTitle]: data
-                            });
-                        }
+                    Promise.all(promises).then((reportDataArray, error) => {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            for (let i = 0; i < reportDataArray.length; i++) {
+                                var data = reportDataArray[i].rows;
+                                var category = Object.keys(categories[i])[0];
+                                var tableTitle = Object.values(categories[i])[0];
 
-                        // create report pdf
-                        const fileInfo = juriHelper.makeJurisdictionReport(queryArgs, reportData);
-                        fileInfo.then((createdFile) => {
-                            //console.log(createdFile)
-                            reply.send({ url: createdFile.fileName });
-                        }).catch((error) => {
-                            //console.log("report error");
-                            //console.log(error);
-                        })
+                                reportData[category].push({
+                                    [tableTitle]: data
+                                });
+                            }
+
+                            // create report pdf
+                            const fileInfo = juriHelper.makeJurisdictionReport(queryArgs, reportData);
+                            fileInfo
+                                .then((createdFile) => {
+                                    //console.log(createdFile)
+                                    // reply.send({ url: createdFile.fileName });
+
+                                    // let filePath = path.join(__dirname, "youe-file.whatever");
+
+                                    // const report = fs.readFileSync(createdFile.savePath);
+                                    // reply.type('pdf').send(report);
+
+                                    const fs = require('fs');
+                                    const stream = fs.createReadStream(createdFile.savePath, 'binary');
+
+                                    reply.header('Content-Type', 'application/pdf');
+                                    reply.send(stream).type('application/pdf').code(200);
+                                })
+                                .catch((error) => {
+                                    console.log('report error');
+                                    console.log(error);
+                                });
+                        }
                     });
                 }
             }
         }
-    })
+    });
     next();
-}
+};
 
-module.exports.autoPrefix = '/v1'
+module.exports.autoPrefix = '/v1';

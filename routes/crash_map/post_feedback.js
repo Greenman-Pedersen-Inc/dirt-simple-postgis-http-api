b2d61ec69486f606696ed6ec7f109365e69fa52c
snopachinda-gpi <snopachinda@gpinet.com>
2022-05-18 11:22:55 -0700

    user feedback updates

--- routes/crash_map/post_feedback.js
+++ routes/crash_map/post_feedback.js
@@ -37,9 +37,14 @@ const getQuery = (queryArgs) => {
     ];
 
     if (queryArgs.hasImage) { 
-        params.push('image_byte_array');
-        valuesParams.push('$' + (valuesParams.length));
-        values.push(queryArgs['imageUri']);
+        const valuesParamLength = valuesParams.length;
+        params.push('image_byte_array_string');
+        valuesParams.push('$' + (valuesParamLength + 1));
+        values.push(decodeURI(queryArgs['imageUri']));
+
+        // params.push('image_type');
+        // valuesParams.push('$' + (valuesParamLength + 2));
+        // values.push(queryArgs['image_type']);
     }
 
     var sql = `
@@ -64,57 +69,61 @@ const schema = {
     description: 'POSTs user feedback and data to the database',
     tags: ['crash-map'],
     summary: 'POSTs user feedback and data to the database',
-    params: {
-        username: {
-            type: 'string',
-            description: 'User email to log into SV',
-            example: 'snopachinda@gpinet.com'
-        },
-        label: {
-            type: 'string',
-            description: 'Category tag: bug, wrong location, help wanted, question, enchancement, other',
-            example: 'bug'
-        },
-        title: {
-            type: 'string',
-            description: 'Title of the feedback form',
-            example: 'Incorrect location in NJTR-1 form data'
-        },
-        open_location: {
-            type: 'string',
-            description: 'Location where the user opened the feedback form',
-            example: 'Crash Map'
-        },
-        description: {
-            type: 'string',
-            description: 'Detailed description of the feedback or bug',
-            example: 'The county should be Atlantic, not burlington.'
-        },
-        crash_description: {
-            type: 'string',
-            description: 'list of crash IDs',
-            example: '13-19-2021-21-39253-AC, ...'
-        },
-        filter_description: {
-            type: 'string',
-            description: 'human readable string of applied filters',
-            example: 'Date: (2017,2018,2019,2020,2021)'
-        },
-        crash_filter: {
-            type: 'string',
-            description:
-                'stringified JSON of crash filter object. ex: {"mp_start": "0", "mp_end": "11.6", "year": "2017,2018,2019", "contr_circum_code_vehicles": "01"}',
-            example: '{"mp_start": "0", "mp_end": "11.6", "year": "2017,2018,2019", "contr_circum_code_vehicles": "01"}'
-        },
-        imageUri: {
-            type: 'string',
-            description: 'screenshot URI',
-            default: ''
-        },
-        hasImage: {
-            type: 'boolean',
-            description: 'a screenshot is attached',
-            default: false
+    body: {
+        type: 'object',
+        properties: {
+            imageUri: { type: 'string' },
+            username: {
+                type: 'string',
+                description: 'User email to log into SV',
+                example: 'snopachinda@gpinet.com'
+            },
+            label: {
+                type: 'string',
+                description: 'Category tag: bug, wrong location, help wanted, question, enchancement, other',
+                example: 'bug'
+            },
+            title: {
+                type: 'string',
+                description: 'Title of the feedback form',
+                example: 'Incorrect location in NJTR-1 form data'
+            },
+            open_location: {
+                type: 'string',
+                description: 'Location where the user opened the feedback form',
+                example: 'Crash Map'
+            },
+            description: {
+                type: 'string',
+                description: 'Detailed description of the feedback or bug',
+                example: 'The county should be Atlantic, not burlington.'
+            },
+            crash_description: {
+                type: 'string',
+                description: 'list of crash IDs',
+                example: '13-19-2021-21-39253-AC, ...'
+            },
+            filter_description: {
+                type: 'string',
+                description: 'human readable string of applied filters',
+                example: 'Date: (2017,2018,2019,2020,2021)'
+            },
+            crash_filter: {
+                type: 'string',
+                description:
+                    'stringified JSON of crash filter object. ex: {"mp_start": "0", "mp_end": "11.6", "year": "2017,2018,2019", "contr_circum_code_vehicles": "01"}',
+                example: '{"mp_start": "0", "mp_end": "11.6", "year": "2017,2018,2019", "contr_circum_code_vehicles": "01"}'
+            },
+            hasImage: {
+                type: 'boolean',
+                description: 'a screenshot is attached',
+                default: false
+            },
+            // image_type: {
+            //     type: 'string',
+            //     description: 'header when getting the image base64 value',
+            //     example: 'data:image/png;base64'
+            // }
         }
     }
 };
@@ -129,8 +138,7 @@ module.exports = function (fastify, opts, next) {
         schema: schema,
         preHandler: fastify.auth([fastify.verifyToken]),
         handler: function (request, reply) {
-            // const queryArgs = request.query;
-            const queryArgs = JSON.parse(request.headers.body);
+            const queryArgs = request.body;
 
             function onConnect(err, client, release) {
                 if (err) {
@@ -140,21 +148,26 @@ module.exports = function (fastify, opts, next) {
                         error: 'Internal Server Error',
                         message: 'unable to connect to database server'
                     });
-                } else if (queryArgs.username == undefined) {
+                }
+                else if (queryArgs.username == undefined) {
                     return reply.send({
                         statusCode: 500,
                         error: 'Internal Server Error',
                         message: 'need user name'
                     });
-                } else {
+                }
+                else {
                     try {
                         const queryParams = getQuery(queryArgs);
                         client.query(queryParams.query, queryParams.values, function onResult(err, result) {
                             release();
-                            var result = {};
-                            if (err) result = { success: false, error: err };
-                            else result = { success: true };
-                            reply.send(err || result);
+                            var resultParams = {};
+                            if (err) resultParams = { success: false, error: err };
+                            else if (result.rowCount > 0) {
+                                resultParams = { success: true };
+                            }
+                            else resultParams = { success: false};
+                            reply.send(err || resultParams);
                         });
                     } catch (error) {
                         release();

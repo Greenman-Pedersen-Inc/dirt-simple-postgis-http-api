fb5c1a01a626a350e8a6260ef1d66b51aac73e3c
mcollins-GPI <7659129+mcollins-GPI@users.noreply.github.com>
2022-08-10 00:10:39 +0000

    update trends route

--- helper_functions/trends_helper.js
+++ helper_functions/trends_helper.js
@@ -1,6 +1,5 @@
-
 // *---------------*
-// SQL formatters 
+// SQL formatters
 // *---------------*
 
 // returns an array of category strings to query for
@@ -12,24 +11,24 @@ function getQueryCategories(juriLevel) {
 
 // returns a list of county codes for an MPO based on the jurisdiction value
 function getMpoCounties(juriValue) {
-    if (juriValue.toLowerCase() === 'njtpa') return "02,07,09,10,12,13,14,15,16,18,19,20,21";
-    else if (juriValue.toLowerCase() === 'sjtpo') return "01,05,06,17";
-    else if (juriValue.toLowerCase() === 'dvrpc') return "03,04,08,11";
+    if (juriValue.toLowerCase() === 'njtpa') return '02,07,09,10,12,13,14,15,16,18,19,20,21';
+    else if (juriValue.toLowerCase() === 'sjtpo') return '01,05,06,17';
+    else if (juriValue.toLowerCase() === 'dvrpc') return '03,04,08,11';
 }
 
 // gets the MPO category based on the county code
 function getMpoByCounty(countyCode) {
-    const njtpa = "02,07,09,10,12,13,14,15,16,18,19,20,21";
-    const sjtpo = "01,05,06,17";
-    const dvrpc = "03,04,08,11";
+    const njtpa = '02,07,09,10,12,13,14,15,16,18,19,20,21';
+    const sjtpo = '01,05,06,17';
+    const dvrpc = '03,04,08,11';
     if (njtpa.indexOf(countyCode) >= 0) return njtpa;
     else if (sjtpo.indexOf(countyCode) >= 0) return sjtpo;
     else if (dvrpc.indexOf(countyCode) >= 0) return dvrpc;
 }
 
 function getTableNameByAttribute(attribute) {
-    if (attribute === "intersections") return "trends.crash_accident_crs_strt_intrsctn_code_y";
-    else if (attribute === "cellphones") return "trends.crash_accident_cell_phone_code_y";
+    if (attribute === 'intersections') return 'trends.crash_accident_crs_strt_intrsctn_code_y';
+    else if (attribute === 'cellphones') return 'trends.crash_accident_cell_phone_code_y';
 }
 
 // Splits a code string by "," to return an array of codes
@@ -47,40 +46,42 @@ function splitCodes(codeString) {
 // EXAMPLE: enviornmentCode = "01,02,03"
 // RETURNS: "'01','02','03'"
 function formatCodes(codeString) {
-    var returnCodes = "";
+    var returnCodes = '';
     var codes = splitCodes(codeString);
     if (codes.length > 0) {
         var formattedCodes = [];
-        codes.forEach(splitCode => {
+        codes.forEach((splitCode) => {
             formattedCodes.push("'" + splitCode + "'");
         });
-        returnCodes = formattedCodes.join(", ");
+        returnCodes = formattedCodes.join(', ');
     }
     return returnCodes;
 }
 
 function createWhereClause(queryCategory, queryArgs) {
     var whereClauses = [];
-    if (queryArgs.startYear && queryArgs.endYear) whereClauses.push(`yr_num BETWEEN ${queryArgs.startYear} AND ${queryArgs.endYear}`);
-    if (queryArgs.startTime !== '0000' && queryArgs.endTime !== '2400') whereClauses.push(`(hour >= '${queryArgs.startTime}' and hour <= '${queryArgs.endTime}')`);
+    if (queryArgs.startYear && queryArgs.endYear)
+        whereClauses.push(`yr_num BETWEEN ${queryArgs.startYear} AND ${queryArgs.endYear}`);
+    if (queryArgs.startTime) whereClauses.push(`(hour >= '${queryArgs.startTime.substring(0, 2)}')`);
+    if (queryArgs.endTime) whereClauses.push(`(hour <= '${queryArgs.endTime.substring(0, 2)}')`);
     if (queryArgs.crashType !== '00') whereClauses.push(`crash_type = '${queryArgs.crashType}'`);
 
     if (queryCategory === 'mpo') {
-        if (queryArgs.jurisdictionLevel.toLowerCase() === 'county' || queryArgs.jurisdictionLevel.toLowerCase() === 'municipality') {
-            const codeList = getMpoByCounty(queryArgs.jurisdictionValue.substring(0,2));
-            whereClauses.push(`cnty_code IN (${formatCodes(codeList)})`);   
-        }
-        else {
+        if (
+            queryArgs.jurisdictionLevel.toLowerCase() === 'county' ||
+            queryArgs.jurisdictionLevel.toLowerCase() === 'municipality'
+        ) {
+            const codeList = getMpoByCounty(queryArgs.jurisdictionValue.substring(0, 2));
+            whereClauses.push(`cnty_code IN (${formatCodes(codeList)})`);
+        } else {
             var countyCode = getMpoCounties(queryArgs.jurisdictionValue);
-            whereClauses.push(`cnty_code IN (${formatCodes(countyCode)})`);            
+            whereClauses.push(`cnty_code IN (${formatCodes(countyCode)})`);
         }
-    }
-    else if (queryCategory === 'county') {
-        whereClauses.push(`cnty_code = '${queryArgs.jurisdictionValue.substring(0,2)}'`);
-    }
-    else if (queryCategory === 'municipality') {
-        whereClauses.push(`cnty_code = '${queryArgs.jurisdictionValue.substring(0,2)}'`);
-        whereClauses.push(`mncplty_code = '${queryArgs.jurisdictionValue.substring(2,4)}'`);
+    } else if (queryCategory === 'county') {
+        whereClauses.push(`cnty_code = '${queryArgs.jurisdictionValue.substring(0, 2)}'`);
+    } else if (queryCategory === 'municipality') {
+        whereClauses.push(`cnty_code = '${queryArgs.jurisdictionValue.substring(0, 2)}'`);
+        whereClauses.push(`mncplty_code = '${queryArgs.jurisdictionValue.substring(2, 4)}'`);
     }
     return whereClauses.join(' AND ');
 }
@@ -88,15 +89,13 @@ function createWhereClause(queryCategory, queryArgs) {
 function createGroupByClause(queryCategory) {
     if (queryCategory === 'county') {
         return 'yr_num, cnty_code';
-    }
-    else if (queryCategory === 'municipality') {
+    } else if (queryCategory === 'municipality') {
         return 'yr_num, cnty_code, mncplty_code';
-    }
-    else return 'yr_num';
+    } else return 'yr_num';
 }
 
 // *---------------*
-// SQL query makers 
+// SQL query makers
 // *---------------*
 
 function createQuery(queryCategory, queryArgs, tableName) {
@@ -127,13 +126,12 @@ function getTrendsQueryObject(queryArgs, tableName) {
     const categories = getQueryCategories(queryArgs.jurisdictionLevel);
     // console.log(categories)
     var returnObj = {};
-    categories.forEach(category => {
-        const sql = createQuery(category, queryArgs, tableName)
-        returnObj[category] = {query: sql};
+    categories.forEach((category) => {
+        const sql = createQuery(category, queryArgs, tableName);
+        returnObj[category] = { query: sql };
     });
     return returnObj;
 }
-
 
 // *---------------*
 // Module Exports
